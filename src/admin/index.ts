130d7e6b64e11c13730d0d90d750a0be5428c4c3
Antonio Sanches <sysadmin@metamorph-ai.com>
2026-04-22 05:25:22 +0000

    feat(admin): wire L1.5 subpath re-exports

--- src/admin/index.ts
+++ src/admin/index.ts
@@ -1,1 +1,4 @@
-export {};
+export { getSupabaseAdminClient } from "./client";
+export type { GetAdminClientOptions } from "./client";
+
+export { generateRecoveryLink, generateInviteLink, AuthAdminError } from "./links";
