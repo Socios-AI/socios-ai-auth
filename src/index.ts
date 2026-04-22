61a87754cf8f2b4e68877b306c05881cb8c4e269
Antonio Sanches <sysadmin@metamorph-ai.com>
2026-04-22 05:20:44 +0000

    feat(index): wire L1 subpath re-exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-export {};
+export { validateFromParam, deriveAppName } from "./from-param";
+export type { FromValidationResult } from "./from-param";
+
+export { passwordSchema, resetFormSchema } from "./validation";
+export type { ResetFormInput } from "./validation";
+
+export { getSupabaseBrowserClient } from "./browser/client";
+export type { GetBrowserClientOptions } from "./browser/client";
+
+export type { AuthErrorCode } from "./types";
