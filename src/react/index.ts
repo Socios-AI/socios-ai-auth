3cf9df8ec629f1c4e2b00b226f6b6063df898a70
Antonio Sanches <sysadmin@metamorph-ai.com>
2026-04-22 05:36:37 +0000

    feat(react): wire L2 subpath re-exports

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,1 +1,13 @@
-export {};
+export { useTokenConsumption } from "./useTokenConsumption";
+export type { UseTokenConsumptionOptions } from "./useTokenConsumption";
+
+export { useResetPassword } from "./useResetPassword";
+export type { UseResetPasswordOptions } from "./useResetPassword";
+
+export { useSetPassword } from "./useSetPassword";
+export type { UseSetPasswordOptions } from "./useSetPassword";
+
+export { useVerify } from "./useVerify";
+export type { UseVerifyOptions } from "./useVerify";
+
+export type { AuthErrorCode } from "../types";
